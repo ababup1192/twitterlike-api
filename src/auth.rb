2c598487d246a21d14f013f85f30998dacfaefe7
ababup1192 <ababup1192@gmail.com>
2016-09-26 19:13:05 +0900

    Implement generate hashed password function

--- src/auth.rb
+++ src/auth.rb
@@ -3,6 +3,18 @@ require 'securerandom'
 
 # Authenticate utility module
 module Auth
+  # ハッシュ化されたパスワードを生成する
+  def self.generate_hashed_password(password)
+    OpenSSL::Digest::SHA256.digest(password)
+  end
+
+  # ストレッチする
+  def self.stretch(password)
+    (1..1000).reduce(password) do |hashed_pass, _|
+      Auth.generate_hashed_password(hashed_pass)
+    end
+  end
+
   # パスワードにソルトをかける
   def self.splinkle_salt(password, salt_length = 16)
     salt = SecureRandom.base64(salt_length)
@@ -10,8 +22,15 @@ module Auth
     { password: password_with_salt, salt: salt }
   end
 
-  # ハッシュ化されたパスワードを生成する
-  def self.generate_hashed_password(password)
-    OpenSSL::Digest::SHA256.digest(password)
+  # ソルト付きパスワードを生成する
+  def self.generate_hashed_password_with_salt(password, salt_length = 16)
+    pass_with_salt = Auth.splinkle_salt(password, salt_length)
+    hashed_pass = Auth.stretch(pass_with_salt[:password])
+    pass_with_salt.update(password: hashed_pass)
+  end
+
+  # ソルトとパスワードを渡して、ハッシュを得る
+  def self.hashed_password(password, salt)
+    Auth.stretch(password + salt)
   end
 end
--- test/auth_test.rb
+++ test/auth_test.rb
@@ -4,6 +4,25 @@ require_relative '../src/auth'
 
 # AuthTestClass
 class AuthTest < Test::Unit::TestCase
+  def test_generate_hashed_password
+    password = 'password'
+    hashed_pass = Auth.generate_hashed_password(password)
+
+    p hashed_pass
+
+    assert_not_equal password, hashed_pass
+  end
+
+  def test_stretch
+    password = 'password'
+    stretched_pass = Auth.stretch(password)
+    hashed_pass = Auth.generate_hashed_password(password)
+
+    p stretched_pass
+
+    assert_not_equal hashed_pass, stretched_pass
+  end
+
   def test_splinkle_salt_length
     password = 'ps'
     pass_with_salt = Auth.splinkle_salt(password)
@@ -26,12 +45,21 @@ class AuthTest < Test::Unit::TestCase
     assert_equal expected, actual
   end
 
-  def test_generate_hashed_password
+  def test_generate_hashed_password_with_salt
     password = 'password'
-    hashed_pass = Auth.generate_hashed_password(password)
+    pass_with_salt = Auth.generate_hashed_password_with_salt(password)
 
-    p hashed_pass
+    p pass_with_salt
 
-    assert_not_equal password, hashed_pass
+    assert_not_equal password, pass_with_salt[:password]
+  end
+
+  def test_hashed_pass
+    password = 'password'
+    pass_with_salt = Auth.generate_hashed_password_with_salt(password)
+    expected = pass_with_salt[:password]
+    actual = Auth.hashed_password(password, pass_with_salt[:salt])
+
+    assert_equal expected, actual
   end
 end
