0eee4e3b78a2cbcf88a21b97a2ea7070b4d6de8b
ababup1192 <ababup1192@gmail.com>
2016-09-27 11:26:12 +0900

    Implement json User token auth

--- src/app.rb
+++ src/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require_relative 'user'
+require_relative 'session'
 
 # Sinatra Main controller
 class MainApp < Sinatra::Base
@@ -15,6 +16,10 @@ class MainApp < Sinatra::Base
   get '/user' do
     users = User.new.db
     users.all.to_json
+  end
+  get '/session' do
+    sessions = Session.new.db
+    sessions.all.to_json
   end
   post '/user', provides: :json do
     json_hash = JSON.parse(request.body.read, symbolize_names: true)
@@ -30,6 +35,17 @@ class MainApp < Sinatra::Base
   post '/user/auth', provides: :json do
     json_hash = JSON.parse(request.body.read, symbolize_names: true)
     status, message = User.new.auth(json_hash)
+    case status
+    when :ok
+      message
+    when :error
+      status(401)
+      message
+    end
+  end
+  post '/user/auth/token', provides: :json do
+    json_hash = JSON.parse(request.body.read, symbolize_names: true)
+    status, message = User.new.auth_token(json_hash)
     case status
     when :ok
       message
