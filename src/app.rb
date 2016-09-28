87cd14af60c121c36cf972e962bb2d0ffa4ab749
ababup1192 <ababup1192@gmail.com>
2016-09-28 12:34:57 +0900

    Implement Follow Router

--- src/app.rb
+++ src/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'user_router'
 require_relative 'tweet_router'
+require_relative 'follow_router'
 
 # Sinatra Main controller
 class MainApp < Sinatra::Base
@@ -13,4 +14,5 @@ class MainApp < Sinatra::Base
 
   use UserRouter
   use TweetRouter
+  use FollowRouter
 end
---
+++ src/follow_router.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/json'
+require 'json'
+require_relative 'utils/http_helper'
+require_relative 'entity/follow'
+
+# Sinatra Follow controller
+class FollowRouter < Sinatra::Base
+  include HttpHelper
+
+  get '/follows' do
+    follows = Follow.new.db
+    json follows.all
+  end
+
+  get '/follows/:id' do
+    result = Follow.new.find(params[:id].to_i)
+    create_response(result)
+  end
+
+  post '/follows', provides: :json do
+    result = Follow.new.save(json_request)
+    create_response(result)
+  end
+end
